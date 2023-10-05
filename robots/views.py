174efcf9edb381f2b28318aebb2270a6a25b4e23
David-Roklem <roklem@mail.ru>
2023-10-05 18:22:35 +0300

    Add second data validation function

--- robots/views.py
+++ robots/views.py
@@ -2,23 +2,18 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Robot
-from robots.utils.validators import check_request_data
+from robots.utils.validators import check_correct_values
 
 
 @csrf_exempt
 def create_new_robot(request):
     if request.method == 'POST':
-        data = check_request_data(request)
+        data = check_correct_values(request)
         model = data.get('model')
         version = data.get('version')
         created = data.get('created')
-
-        if model and version and created:
-            robot = Robot(model=model, version=version, created=created)
-            robot.save()
-            return JsonResponse({'message': 'Robot created successfully'})
-        else:
-            return JsonResponse({'message': 'Invalid data provided'})
-
+        robot = Robot(model=model, version=version, created=created)
+        robot.save()
+        return JsonResponse({'message': 'Robot created successfully'})
     else:
         return JsonResponse({'message': 'Invalid request method'})
