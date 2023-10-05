b8b96078945354b7fdbe5242d270223e4264c76a
David-Roklem <roklem@mail.ru>
2023-10-05 14:28:47 +0300

    Add view function for robots
    Alongside with the main view function for robots app
    comes some simple data validation

--- robots/views.py
+++ robots/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Robot
+from robots.utils.validators import check_request_data
 
-# Create your views here.
+
+@csrf_exempt
+def create_new_robot(request):
+    if request.method == 'POST':
+        data = check_request_data(request)
+        model = data.get('model')
+        version = data.get('version')
+        created = data.get('created')
+
+        if model and version and created:
+            robot = Robot(model=model, version=version, created=created)
+            robot.save()
+            return JsonResponse({'message': 'Robot created successfully'})
+        else:
+            return JsonResponse({'message': 'Invalid data provided'})
+
+    else:
+        return JsonResponse({'message': 'Invalid request method'})
