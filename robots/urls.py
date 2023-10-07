333a7d5533d7e6dc9dd2bdc5099a664aba4f9a95
David-Roklem <roklem@mail.ru>
2023-10-07 21:05:21 +0300

    Add view func that generate an exel report

--- robots/urls.py
+++ robots/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from robots.views import create_new_robot
+from robots.views import create_new_robot, generate_report_view
 
 urlpatterns = [
     path('new-robot/', create_new_robot, name='create_new_robot'),
+    path('download-report/', generate_report_view, name='download_report'),
 ]
