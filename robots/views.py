bb101d8c1641505c07efb0876fac6c988f3b4b8f
David-Roklem <roklem@mail.ru>
2023-10-07 21:05:21 +0300

    Add view func that generate an exel report

--- robots/views.py
+++ robots/views.py
@@ -1,10 +1,12 @@
 from http import HTTPStatus
+import os
 
 # from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, FileResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Robot
 from robots.utils.validators import check_correct_values
+from django.views.decorators.http import require_GET
 
 from .utils.exel_report import generate_excel_report
 
@@ -14,7 +16,6 @@ def create_new_robot(request):
     '''
     API interface that handles JSON request data and
     provide an informative response to the sender
-
     '''
     if request.method == 'POST':
         try:
@@ -41,4 +42,27 @@ def create_new_robot(request):
             {"status": "error", "message": HTTPStatus.
              METHOD_NOT_ALLOWED.phrase},
             status=HTTPStatus.METHOD_NOT_ALLOWED,
+        )
+
+
+@require_GET
+def generate_report_view(request):
+    '''
+    API interface that handles JSON request data and
+    provide an informative response to the sender
+    '''
+    try:
+        generate_excel_report()
+        file_path = 'robot_report.xlsx'
+        if os.path.exists(file_path):
+            return FileResponse(open(file_path, 'rb'), status=HTTPStatus.OK)
+        else:
+            return JsonResponse(
+                {'status': 'error', 'message': 'Exel report file is missing'},
+                status=HTTPStatus.HTTP_404_NOT_FOUND,
+            )
+    except AttributeError as e:
+        return JsonResponse(
+            {'error': str(e)},
+            status=HTTPStatus.HTTP_500_INTERNAL_SERVER_ERROR
         )
